64e0d5b826d025a78bd632c78a7e9bf2d5736f9b
Minh Kiet <kietdk@gmail.com>
2021-04-08 15:38:49 +0700

    update code

--- src/main/java/com/example/demo/domain/Contact.java
+++ src/main/java/com/example/demo/domain/Contact.java
@@ -17,7 +17,7 @@ public class Contact implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
 	private int id;
 
--- src/main/java/com/example/demo/repository/ContactRepository.java
+++ src/main/java/com/example/demo/repository/ContactRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Contact;
 
+@Repository
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
 	List<Contact> findByNameContaining(String q);
 }
